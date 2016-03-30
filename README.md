9208f5130ce94ae432b0a20438699647cd419596
Christopher Swenson <chris@caswenson.com>
2016-03-30 11:32:37 -0700

    Add initial version

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # tito-zapier
 ti.to webhook to zapier connector using AWS Lambda
+
+## Instructions
+
+Modify `hook.py` to add your zapier incoming webhook, and upload this code to AWS Lambda (POST, open authentication).
+
+## Google Sheets
+
+If you want to use Google Sheets, you can use the following header row:
+
+```
+title	url	name	email	payment email	currency	total cost	ticket discount code	ticket quantity	ticket cost
+```
+
+## License
+
+MIT License.
---
+++ hook.py
@@ -0,0 +1,41 @@
+import json
+import logging
+import urllib
+import urllib2
+
+zapier_incoming = ''
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    reg = event["registration"]
+    name = reg["name"]
+    email = event["email"]
+    currency = reg["currency"]
+    total_cost = reg["total"]
+    ticket_discount_code = event["discount_code_used"]
+    ticket_quantity = sum(r["quantity"] for r in reg["receipt"]["receipt_lines"])
+    ticket_cost = event["price"]
+    payment_email = reg["email"]
+
+    body = dict(
+      title=event["event"]["title"],
+      url=event["event"]["url"],
+      name=name,
+      email=email,
+      payment_email=payment_email,
+      currency=currency,
+      total_cost=total_cost,
+      ticket_discount_code=ticket_discount_code,
+      ticket_quantity=ticket_quantity,
+      ticket_cost=ticket_cost)
+
+    data = urllib.urlencode(body)
+
+    returned = urllib2.urlopen(zapier_incoming, data)
+    try:
+      return returned.read()
+    finally:
+      returned.close()
